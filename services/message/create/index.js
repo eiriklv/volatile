ed92deef01c64a7687ee6453ce77c001dd555525
Eirik Vullum <evullum@gmail.com>
2014-08-28 00:17:02 +0200

    use https / SSL

--- services/message/create/index.js
+++ services/message/create/index.js
@@ -9,7 +9,7 @@ exports = module.exports = function(Message, helpers, config) {
         message.save(function(err, product) {
             if (err || !product.hash) return callback(err ? err : 'no hash returned');
 
-            var returnUrl = 'http://' + config.get('server.domain') + '/' + product.hash;
+            var returnUrl = 'https://' + config.get('server.domain') + '/' + product.hash;
             callback(null, returnUrl);
         });
     };
